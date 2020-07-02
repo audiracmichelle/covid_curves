7139ec461f0a963e14b5d1c638827261e244bd74
Mauricio <mauriciogtec@gmail.com>
2020-07-02 14:54:05 -0500

    output of alternative two way model

---
+++ hockey_stick_no3way_alternative2_backup/roll_deaths/model.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(magrittr)
+library(feather)
+library(rstanarm)
+
+#### #### 
+## model
+
+## Read county_train
+county_train <- read_feather("../../county_train.feather") %>%
+  mutate(t = days_since_thresh, t_squared=days_since_thresh^2)
+
+## define y
+county_train$y <- county_train$roll_deaths
+#dim(county_train)
+county_train %<>% 
+  filter(!is.na(roll_deaths))
+#dim(county_train)
+
+## Train model
+options(mc.cores=2)
+model = stan_glmer.nb(
+  y ~
+    # global mean and random effects
+    poly(days_since_thresh, 2) + (poly(days_since_thresh, 2) | fips) +
+    # interaction without mean shift
+    t:intervention + t_squared:intervention +
+    # nchs
+    nchs + t:nchs + t_squared:nchs +
+    # effect of intervention speed +
+    speed_btwn_copy +
+    t:intervention:days_btwn_stayhome_thresh +
+    t_squared:intervention:days_btwn_stayhome_thresh
+  ,
+  offset = log(pop),
+  data=county_train,
+  algorithm="mean",
+  iter = 100000,
+  adapt_iter = 5000,
+  QR=TRUE
+)
+
+saveRDS(model, paste("./model.rds", sep = ""))
+
+#### #### 
+## county_fit
+
+county_fit <- model %>%
+  posterior_predict(county_train, draws = 500)
+
+saveRDS(county_fit, "./county_fit.rds")
