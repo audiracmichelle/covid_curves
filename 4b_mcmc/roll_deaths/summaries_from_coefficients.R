298f1e45bc234356a6b22d1616c207a46ca54c18
Mauricio <mauriciogtec@gmail.com>
2020-09-01 16:35:50 -0500

    aggregated stats

--- 4b_mcmc/roll_deaths/summaries_from_coefficients.R
+++ 4b_mcmc/roll_deaths/summaries_from_coefficients.R
@@ -6,6 +6,7 @@ library(gridExtra)
 library(cowplot)
 library(ggthemes)
 library(xtable)
+library(lubridate)
 
 
 #' @title Curves from Stan model coefficients
@@ -122,22 +123,46 @@ curves_by_fips = function(
       sprintf("b[poly(days_since_thresh, 2)%s fips:%s]", 1:2, f)
     )
 
+    # extract stats
+    # 1. days to peak
+    # 2. value at peak
+    # 3. cum deaths at day 20, 30
+    # 4. value in May 1
+
     resid = samples[ ,nms] %*% t(poly_t) 
     lp = covar_effect + interv_effect + resid
+    predicted_mean = f_pop * exp(lp)
+    
     peak_pos = apply(lp, 1, which.max) - 1
     peak_val = 1e6 * exp(apply(lp, 1, max))
     
+    cum_deaths = t(apply(predicted_mean, 1, cumsum))
+    dates = f_data$date
+    may_1 = which(dates == "2020-05-01")
+    if (dates[1] > "2020-05-01") {
+      cum_deaths_may_1 = matrix(0, 1000, 1)
+      may_1_t = NA
+    } else {
+      day_diff = min(max_t, as.integer(ymd("2020-05-01") - ymd(dates[1])))
+      cum_deaths_may_1 = cum_deaths[ ,day_diff]
+      may_1_t = day_diff
+    }
+    
     out = list(
       data = f_data,
+      f_pop = f_pop,
+      may_1_t=may_1_t,
+      cum_deaths = cum_deaths,
+      cum_deaths_may_1 = cum_deaths_may_1,
       intervention_curve = interv_effect,
       random_effect_curve = resid,
       covar_curve = covar_effect,
       linear_predictor = lp,
       disp = samples[ ,"reciprocal_dispersion"],
       peak_pos = peak_pos,
-      peak_val = peak_val
+      peak_val = peak_val,
+      predicted_mean = predicted_mean
     )
-    out$predicted_mean = f_pop * exp(out$linear_predictor)
     results[[f]] = out
     progress_bar$tick()
   }
@@ -282,30 +307,112 @@ nchs_effect_summaries = function(extracted_curves) {
   
   # Strategy 2: Aggregate lp curves per sample
   agg_lp = array(0, c(1000, max_t + 1, 6))
+  agg_cumdeaths = array(0, c(1000, max_t + 1, 6))
+  agg_cumdeaths_may_1 = array(0, c(1000, 6))
   nchs_count = table(nchs)
+  total_pop = numeric(6)
+  total_pop_may1 = numeric(6)
+
   for (i in seq_along(nchs)) {
     k = as.integer(nchs[i])
     n = nchs_count[k]
     lp = extracted_curves[[i]]$linear_predictor
+    
     agg_lp[ , , k] = agg_lp[ , , k] + lp / n
+    
+    f_pop = extracted_curves[[i]]$f_pop
+    cumdeaths = extracted_curves[[i]]$cum_deaths
+
+    agg_cumdeaths[ , , k] = agg_cumdeaths[ , , k] + cumdeaths
+    total_pop[k] = total_pop[k] + f_pop
+
+    may_1_t = extracted_curves[[i]]$may_1_t
+    if (!is.na(may_1_t)) {
+      cumdeaths_may_1 = cumdeaths[ ,may_1_t]
+      agg_cumdeaths_may_1[ , k] = agg_cumdeaths_may_1[ , k] + cumdeaths_may_1
+      total_pop_may1[k] = total_pop_may1[k] + f_pop
+    }
+  }
+  
+  for (j in 1:6) {
+    agg_cumdeaths[ , ,j] = agg_cumdeaths[ , ,j]  #  / total_pop[j]
+    agg_cumdeaths_may_1[ ,j] = agg_cumdeaths_may_1[ ,j]   # / total_pop_may1[j]
   }
   
   summaries$agg_lp = agg_lp
+  summaries$agg_cumdeaths = agg_cumdeaths
+  summaries$agg_cumdeaths_may_1 = agg_cumdeaths_may_1
+
+  return (summaries)
+}
+
+
+
+differences_summaries = function(curve_summary_list, df) {
+  
+  max_t = max(df$days_since_thresh)
+  
+  agg_cumdeaths_late_minus_actual = array(0, c(1000, max_t + 1, 6))
+  agg_cumdeaths_actual_minus_early = array(0, c(1000, max_t + 1, 6))
+  agg_cumdeaths_may_1_late_minus_actual = array(0, c(1000, 6))
+  agg_cumdeaths_may_1_actual_minus_early = array(0, c(1000, 6))
+  
+  nchs = map_chr(curve_summary_list$actual, ~ .x$data$nchs[1])
+  nchs_count = table(nchs)
+  total_pop = numeric(6)
+  total_pop_may1 = numeric(6)
+  
+  summaries = list()
+  
+  for (i in seq_along(nchs)) {
+    k = as.integer(nchs[i])
+    n = nchs_count[k]
+    cd_actual = curve_summary_list$actual[[i]]$cum_deaths
+    cd_late = curve_summary_list$late[[i]]$cum_deaths
+    cd_early = curve_summary_list$early[[i]]$cum_deaths
+    
+    f_pop = curve_summary_list$actual[[i]]$f_pop
+    
+    agg_cumdeaths_late_minus_actual[ , , k] = (
+      agg_cumdeaths_late_minus_actual[ , , k] + (cd_late - cd_actual)
+    )
+    agg_cumdeaths_actual_minus_early[ , , k] = (
+      agg_cumdeaths_actual_minus_early[ , , k] + (cd_actual - cd_early)
+    )
+    total_pop[k] = total_pop[k] + f_pop
+    
+    may_1_t = curve_summary_list$actual[[i]]$may_1_t
+    if (!is.na(may_1_t)) {
+      agg_cumdeaths_may_1_late_minus_actual[ , k] = (
+        agg_cumdeaths_may_1_late_minus_actual[ , k] + (cd_late[ ,may_1_t] - cd_actual[ ,may_1_t])
+      )
+      agg_cumdeaths_may_1_actual_minus_early[ , k] = (
+        agg_cumdeaths_may_1_actual_minus_early[ , k] +(cd_actual[ ,may_1_t] - cd_early[ ,may_1_t])
+      )
+      total_pop_may1[k] = total_pop_may1[k] + f_pop
+    }
+  }
+  
+  summaries$agg_cumdeaths_late_minus_actual = agg_cumdeaths_late_minus_actual
+  summaries$agg_cumdeaths_actual_minus_early = agg_cumdeaths_actual_minus_early
+  summaries$agg_cumdeaths_may_1_late_minus_actual = agg_cumdeaths_may_1_late_minus_actual
+  summaries$agg_cumdeaths_may_1_actual_minus_early = agg_cumdeaths_may_1_actual_minus_early
   
   return (summaries)
 }
+
 
 # Read data
 model = readRDS("./model.rds")
 county_fit = readRDS("./county_fit.rds")
 source("../../plot_foo.R")
 df = read_feather("../../county_train_stayhome.feather")
-
-# === actual intervention ===
+curve_summary_list = list()
 
 # Summary for intervention as observed
 cat("computing curve summaries by fips no offset...\n")
 curve_summaries = curves_by_fips(model, df)
+curve_summary_list[["actual"]] = curve_summary_list
 cat("summarizing by nchs...\n")
 nchs_summaries = nchs_effect_summaries(curve_summaries)
 nchs_summaries_actual = nchs_summaries
@@ -340,6 +447,7 @@ curve_summaries = curves_by_fips(
   df,
   intervention_offset=up
 )
+curve_summary_list[["late"]] = curve_summary_list
 cat("summarizing by nchs...\n")
 nchs_summaries = nchs_effect_summaries(curve_summaries)
 nchs_summaries_late = nchs_summaries
@@ -370,6 +478,7 @@ curve_summaries = curves_by_fips(
   df,
   intervention_offset=down
 )
+curve_summary_list[["early"]] = curve_summary_list
 cat("summarizing by nchs...\n")
 nchs_summaries = nchs_effect_summaries(curve_summaries)
 nchs_summaries_early = nchs_summaries
@@ -467,90 +576,92 @@ summary_list = list(
   "late"=nchs_summaries_late
 )
 
-peaks = list()
+
+cd_diffs = differences_summaries(curve_summary_list, df)
+
+stats = list()
 
 for (j in 1:3) {
   summary_name = names(summary_list)[j]
   summary = summary_list[[j]]
-
-  pos_mean = pos_sd = pos_q05 = pos_q95 = pos_q50 = pos_q25 = pos_q75 = pos_iqr = numeric(6)
-  val_mean = val_sd = val_q05 = val_q95 = val_q50 = val_q25 = val_q75 = val_iqr = numeric(6)
-
+  
+  stats_j = list()
   for (i in 1:6) {
-    D = summary$agg_lp[ , , i]
-    pos = apply(D, 1, which.max) - 1
-    val = 1e6 * exp(apply(D, 1, max))
-    pos_mean[i] = mean(pos)
-    pos_sd[i] = sd(pos)
-    pos_q05[i] = quantile(pos, 0.05)
-    pos_q95[i] = quantile(pos, 0.95)
-    pos_q50[i] = quantile(pos, 0.50)
-    pos_q25[i] = quantile(pos, 0.25)
-    pos_q75[i] = quantile(pos, 0.75)
-    pos_iqr[i] = pos_q75[i] - pos_q25[i]
-    val_mean[i] = mean(val)
-    val_sd[i] = sd(val)
-    val_q05[i] = quantile(val, 0.05)
-    val_q95[i] = quantile(val, 0.95)
-    val_q50[i] = quantile(val, 0.50)
-    val_q25[i] = quantile(val, 0.25)
-    val_q75[i] = quantile(val, 0.75)
-    val_iqr[i] = val_q75[i] - val_q25[i]
+    data_list = list(
+      pos = apply(summary$agg_lp[ , , i], 1, which.max) - 1,
+      val = 1e6 * exp(apply(summary$agg_lp[ , , i], 1, max)),
+      cd10 = summary$agg_cumdeaths[ , 10, i],
+      cd20 = summary$agg_cumdeaths[ , 20, i],
+      cd30 = summary$agg_cumdeaths[ , 30, i],
+      cdmay1 = summary$agg_cumdeaths_may_1[ , i],
+      cd10_lma = cd_diffs$agg_cumdeaths_late_minus_actual[ , 10, i],
+      cd20_lma = cd_diffs$agg_cumdeaths_late_minus_actual[ , 20, i],
+      cd30_lma = cd_diffs$agg_cumdeaths_late_minus_actual[ , 30, i],
+      cdmay1_lma = cd_diffs$agg_cumdeaths_may_1_late_minus_actual[ , i],
+      cd10_ame = cd_diffs$agg_cumdeaths_actual_minus_early[ , 10, i],
+      cd20_ame = cd_diffs$agg_cumdeaths_actual_minus_early[ , 20, i],
+      cd30_ame = cd_diffs$agg_cumdeaths_actual_minus_early[ , 30, i],
+      cdmay1_ame = cd_diffs$agg_cumdeaths_may_1_actual_minus_early[ , i]
+    )
+    
+    for (k in seq_along(data_list)) {
+      D = data_list[[k]]
+      Dname = names(data_list)[k]
+      
+      if (i == 1) { # initialize vectors
+        stats_j[[Dname]] = list()
+        stats_j[[Dname]]$mean = numeric(6)
+        stats_j[[Dname]]$sd = numeric(6)
+        stats_j[[Dname]]$median = numeric(6)
+        stats_j[[Dname]]$iqr = numeric(6)
+        stats_j[[Dname]]$quantiles = matrix(0, 5, 6)
+      }
+      
+      stats_j[[Dname]]$mean[i] = mean(D)
+      stats_j[[Dname]]$sd[i] = sd(D)
+      quants = quantile(D, c(0.05, 0.25, 0.50, 0.75, 0.95))
+      stats_j[[Dname]]$quantiles[ ,i] = quants
+      stats_j[[Dname]]$iqr[i] = quants["75%"] - quants["25%"]
+      stats_j[[Dname]]$median[i] = quants["50%"]
+    }
   }
   
-  peaks[[summary_name]] = list(
-    pos_mean=pos_mean,
-    pos_sd=pos_sd,
-    pos_q05=pos_q05,
-    pos_q95=pos_q95,
-    pos_q50=pos_q50,
-    pos_q25=pos_q25,
-    pos_q75=pos_q75,
-    pos_iqr=pos_iqr,
-    val_mean=val_mean,
-    val_sd=val_sd,
-    val_q05=val_q05,
-    val_q95=val_q95,
-    val_q50=val_q50,
-    val_q25=val_q25,
-    val_q75=val_q75,
-    val_iqr=val_iqr
-  )
+  stats[[summary_name]] = stats_j
   
 }
 
 
-table_peaks  = tibble(
+table_peaks = tibble(
   NCHS=1:6,
   Early_pos=sprintf(
     "%s (%s)",
-    peaks$early$pos_q50,
-    peaks$early$pos_iqr
+    stats$early$pos$median,
+    stats$early$pos$iqr
   ),
   Actual_pos=sprintf(
     "%s (%s)",
-    peaks$actual$pos_q50,
-    peaks$actual$pos_iqr
+    stats$actual$pos$median,
+    stats$actual$pos$iqr
   ),
   Late_pos=sprintf(
     "%s (%s)",
-    peaks$late$pos_q50,
-    peaks$late$pos_iqr
+    stats$late$pos$median,
+    stats$late$pos$iqr
   ),
   Early_val=sprintf(
     "%.2f (%.2f)",
-    peaks$early$val_q50,
-    peaks$early$val_iqr
+    stats$early$val$median,
+    stats$early$val$iqr
   ),
   Actual_val=sprintf(
     "%.2f (%.2f)",
-    peaks$actual$val_q50,
-    peaks$actual$val_iqr
+    stats$actual$val$median,
+    stats$actual$val$iqr
   ),
   Late_val=sprintf(
     "%.2f (%.2f)",
-    peaks$late$val_q50,
-    peaks$late$val_iqr
+    stats$late$val$median,
+    stats$late$val$iqr
   )
 )
 
@@ -581,4 +692,61 @@ print(xtable(table_peaks[ ,-1], include.rownames=FALSE))
 #   )
 # )
 
-/
+# cum deaths stats
+
+nchs_pop = df %>% 
+  select(fips, nchs, pop) %>% 
+  distinct() %>% 
+  group_by(nchs) %>% 
+  summarize(pop=sum(pop)) %>% 
+  pull(pop)
+
+cm_tbl = tibble(NCHS=1:6, pop=nchs_pop)
+for (stat_field in c("cd10", "cd20", "cd30", "cdmay1")) {
+  for (d in c("early", "actual", "late")) {
+    fname = paste(stat_field, d, sep="_")
+    cm_tbl[[fname]] = sprintf(
+      "%.2f [%.2f, %.2f]",
+      stats[[d]][[stat_field]]$median,
+      stats[[d]][[stat_field]]$quantiles[1, ],
+      stats[[d]][[stat_field]]$quantiles[5, ]
+    )
+  }
+}
+cm_tbl
+
+
+cm_tbl2 = tibble(NCHS=1:6, pop=nchs_pop)
+for (stat_field in c("cd10", "cd20", "cd30", "cdmay1")) {
+  for (d in c("early", "actual", "late")) {
+    fname = paste(stat_field, d, sep="_")
+    cm_tbl2[[paste0(fname, "_median")]] = stats[[d]][[stat_field]]$median
+    cm_tbl2[[paste0(fname, "_q05")]] = stats[[d]][[stat_field]]$quantiles[1, ]
+    cm_tbl2[[paste0(fname, "_q95")]] = stats[[d]][[stat_field]]$quantiles[5, ]
+  }
+}
+cm_tbl2
+
+
+write_csv(cm_tbl2, "cumstats_rolldeaths.csv")
+
+
+
+
+write_csv(cm_tbl2, "cumstats_rolldeaths_decrease.csv")
+
+
+cm_tbl3 = tibble(NCHS=1:6, pop=nchs_pop)
+for (stat_field in c("cd10_lma", "cd20_lma", "cd30_lma", "cdmay1_lma", "cd10_ame", "cd20_ame", "cd30_ame", "cdmay1_ame")) {
+  for (d in c("actual")) {
+    fname = stat_field
+    cm_tbl3[[paste0(fname, "_median")]] = stats[[d]][[stat_field]]$median
+    cm_tbl3[[paste0(fname, "_q05")]] = stats[[d]][[stat_field]]$quantiles[1, ]
+    cm_tbl3[[paste0(fname, "_q95")]] = stats[[d]][[stat_field]]$quantiles[5, ]
+  }
+}
+cm_tbl3
+
+write_csv(cm_tbl3, "cumstats_rolldeaths_decrease_diffs.csv")
+
+
